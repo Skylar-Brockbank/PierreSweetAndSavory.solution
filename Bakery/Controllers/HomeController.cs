cb292d17f2f29058d3919a610ae78748fb50076e
Skylar-Brockbank <skylar.brockbank@gmail.com>
2022-01-14 11:47:27 -0800

    bugfix dependency errors

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -1,14 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-
+using Bakery.Models;
 namespace Bakery.Controllers
 {
   public class HomeController : Controller
   {
-    private readonly Skylar_brockbankContext _db;
-    public TreatController(skylar_brockbankContext db)
+    private readonly skylar_brockbankContext _db;
+    public HomeController(skylar_brockbankContext db)
     {
       _db = db;
     }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
--- Bakery/Controllers/TreatController.cs
+++ Bakery/Controllers/TreatController.cs
@@ -1,14 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-
+using Bakery.Models;
 namespace Bakery.Controllers
 {
   public class TreatController : Controller
   {
-    private readonly Skylar_brockbankContext _db;
+    private readonly skylar_brockbankContext _db;
     public TreatController(skylar_brockbankContext db)
     {
       _db = db;
     }
+
     public ActionResult Index()
     {
       return View();
